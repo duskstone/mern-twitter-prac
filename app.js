a55ec522be1584c8bb14d05e307ace362e583d63
duskstone <jedgardromero@gmail.com>
2020-06-15 00:32:24 -0400

    User login and register routes working

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-const port = process.env.PORT || 5000; 
 const mongoose = require('mongoose');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
 const bodyParser = require('body-parser');
 
 mongoose
@@ -12,13 +12,32 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World again"));
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}));
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: "jon",
+        email: "jon@gmail.com",
+        password: "123456"
+    })
+    user.save();
+    res.send("Hello World again")
+});
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
+
+const port = process.env.PORT || 5000; 
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+
+
 
