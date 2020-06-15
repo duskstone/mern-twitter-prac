c1eb50f28f9fe62d7c42976ea3706f4d97d42516
duskstone <jedgardromero@gmail.com>
2020-06-14 21:56:55 -0400

    added test express routes users/tweets

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000; 
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -12,4 +15,10 @@ mongoose
 app.get("/", (req, res) => res.send("Hello World again"));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
