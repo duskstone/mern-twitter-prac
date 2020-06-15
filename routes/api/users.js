7f750df33b3c21d3f734d774328343dfbb72dc8c
duskstone <jedgardromero@gmail.com>
2020-06-15 02:37:37 -0400

    need to fix Tweet Validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,23 @@ const express = require("express");
 const router = express.Router();
 const User = require('../../models/User')
 const bcrypt = require("bcryptjs");
+const keys = require('../../config/keys');
+const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
+
+
 router.post("/register", (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({ email: req.body.email })
     .then(user => {
         if(user) {
@@ -31,6 +44,12 @@ router.post("/register", (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -43,9 +62,24 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch){
-                        res.json({ msg: "Success" });
+                        const payload = {
+                            id: user.id,
+                            handle: user.handle,
+                            email: user.email
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) =>{
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            }
+                            )
                     } else {
-                        res.status(400).json({ password: "Incorrect"});
+                        res.status(400).json({ password: "Incorrect password"});
                     }
                 })
         })
